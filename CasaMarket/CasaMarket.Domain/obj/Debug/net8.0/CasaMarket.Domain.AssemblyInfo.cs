e1d8e7cbe8842f61782eda450b833be83d924d4b
Wfernandez <wandyfernandezperez204@gmail.com>
2025-08-12 20:24:14 -0400

    Here we have already created all the identities

--- CasaMarket/CasaMarket.Domain/obj/Debug/net8.0/CasaMarket.Domain.AssemblyInfo.cs
+++ CasaMarket/CasaMarket.Domain/obj/Debug/net8.0/CasaMarket.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CasaMarket.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d68c92e323c30c003412126c6e23581188bee184")]
 [assembly: System.Reflection.AssemblyProductAttribute("CasaMarket.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CasaMarket.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
